0991d284f2ca9e5e5433a4481e2cb4d9721e5ab7
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-08-16 20:17:05 +0530

    Starting Progres on Creating an Auction

---
+++ src/handlers/createAuction.js
@@ -0,0 +1,11 @@
+const createAuction = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      event,
+      context,
+    }),
+  };
+};
+
+export const handler = createAuction;
