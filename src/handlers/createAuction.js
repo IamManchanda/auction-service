0603c25a9ed4605451e2013e7ca1c864273f8009
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-08-16 22:02:40 +0530

    More progres on creating an auction!

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,10 +1,12 @@
 const createAuction = async (event, context) => {
+  const { title } = JSON.parse(event.body);
+  const status = "Open";
+  const now = new Date();
+  const createdAt = now.toISOString();
+  const auction = { title, status, createdAt };
   return {
-    statusCode: 200,
-    body: JSON.stringify({
-      event,
-      context,
-    }),
+    statusCode: 201,
+    body: JSON.stringify(auction),
   };
 };
 
