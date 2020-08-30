67e00a79555cfa8fe3a67a15459366f202be3aa3
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-08-30 17:19:29 +0530

    DynamoDB Integration!

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,9 +1,23 @@
+import { v4 as uuidv4 } from "uuid";
+import AWS from "aws-sdk";
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
 const createAuction = async (event, context) => {
   const { title } = JSON.parse(event.body);
-  const status = "Open";
   const now = new Date();
-  const createdAt = now.toISOString();
-  const auction = { title, status, createdAt };
+  const auction = {
+    id: uuidv4(),
+    title,
+    status: "Open",
+    created_at: now.toISOString(),
+  };
+  await dynamoDB
+    .put({
+      TableName: "AuctionsTable",
+      Item: auction,
+    })
+    .promise();
   return {
     statusCode: 201,
     body: JSON.stringify(auction),
