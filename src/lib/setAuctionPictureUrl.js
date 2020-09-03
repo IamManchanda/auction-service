6b950f91b585f93341212102fbdf859c3b81dbd8
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-09-04 00:30:45 +0530

    Setting Picture URL in DynamoDB
    Also Validate Auction Ownership

---
+++ src/lib/setAuctionPictureUrl.js
@@ -0,0 +1,19 @@
+import AWS from "aws-sdk";
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+export default async (id, pictureUrl) => {
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Key: {
+      id,
+    },
+    UpdateExpression: "set picture_url = :pictureUrl",
+    ExpressionAttributeValues: {
+      ":pictureUrl": pictureUrl,
+    },
+    ReturnValues: "ALL_NEW",
+  };
+  const result = await dynamodb.update(params).promise();
+  return result.Attributes;
+};
--- src/lib/uploadPictureToS3.js
+++ src/lib/uploadPictureToS3.js
@@ -12,5 +12,5 @@ export default async (key, body) => {
       ContentType: "image/jpeg",
     })
     .promise();
-  return result;
+  return result.Location;
 };
